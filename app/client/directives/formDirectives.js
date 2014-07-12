fd50e715ffbd7a64f66e79b4b8c048a84541e14a
Baudin999 <ckelkboom@gmail.com>
2014-07-12 15:07:11 +0200

    added latex documentation and started writing the control suite for forms

--- app/client/directives/formDirectives.js
+++ app/client/directives/formDirectives.js
@@ -4,6 +4,19 @@ define([], function() {
 
     var module = angular.module('ckForms', []);
 
+
+    /**
+     * TextBox directive. This directives give the power to inject an
+     * input of type text with labels, validation and other playthings
+     * into a form.
+     *
+     * usage:
+     * <ck-text-m title="First name" data-ng-model="myModel.firstName"></ck-text-m>
+     *
+     * attributes:
+     *  cssClass    (changed the icon in the right icon block)
+     *  title:      The value of the label
+      */
     module.directive('ckTextM', function() {
 
         var getTranslation = function(key) {
