2bbfb7b080117bbf57606cd19fddbb2981c59b93
Carlos Kelkboom <ckelkboom@gmail.com>
2014-08-21 08:44:06 +0200

    preparing for the next version of the application

---
+++ app/packaged-app-client/scripts/main.js
@@ -0,0 +1,10 @@
+/**
+ * Created by ckelkboom on 21-8-14.
+ */
+
+
+define([], function() {
+    setTimeout(function() {
+        document.getElementById('main').innerHTML = "Hello my app!!";
+    }, 2000);
+});
